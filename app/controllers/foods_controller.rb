eaa41a906cc8da99dbf2b9d7fbd6b8f9cb923340
levy002 <levy.ukwi002@gmail.com>
2022-07-26 00:18:53 +0200

    Create foods controller with all functionalities

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,42 @@
+class FoodsController < ApplicationController
+  def index
+    @foods = Food.all
+  end
+
+  def show
+    @food = Food.find(params[:id])
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+
+    if @food.save
+      flash[:success] = 'Food was successfully created.'
+      redirect_to foods_path
+    else
+      flash[:error] = 'Error: Failed to add new food!'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+
+    if @food.destroy!
+      flash[:success] = 'Food deleted!'
+      redirect_to foods_path
+    else
+      flash[:error] = 'Failed to delete food!'
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :unit_price)
+  end
+end
