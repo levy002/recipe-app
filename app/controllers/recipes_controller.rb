ecfc447c9a30f2be17e4c80fbcff2d26caca6041
debas-31 <debas31@gmail.com>
2022-07-26 18:45:25 +0300

    Fix linter errors

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,28 +3,26 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
-  def show
-    
-  end
+  def show; end
 
   def create
     @new_recipe = current_user.recipes.new(recipe_params)
     if @new_recipe.save!
       redirect_to recipes_path, flash: { alert: 'Your recipe is saved' }
-    else 
+    else
       redirect_to new_recipe_path, flash: { alert: 'Could not save your recipe' }
     end
   end
-  
+
   def destroy
     @recipe = Recipe.find(params[:id])
     @recipe.destroy!
     flash[:notice] = 'You have deleted the food!'
-      redirect_to recipes_path
+    redirect_to recipes_path
   end
 
   private
-  
+
   def recipe_params
     params.require(:recipe).permit(:name, :prepration_time, :cooking_time, :description)
   end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,10 +1,8 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
-        # Define abilities for the passed in user here. For example:
+    # Define abilities for the passed in user here. For example:
     #
     user ||= User.new # guest user (not logged in)
     can :read, :all
