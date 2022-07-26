3396987847b1e6ef75a613ef57cb18f716c47d65
levy002 <levy.ukwi002@gmail.com>
2022-07-26 15:36:12 +0200

    Permit user parameter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,22 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.all
+  end
+
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path, notice: 'User was successfully created'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
