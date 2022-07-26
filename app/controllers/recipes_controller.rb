9029b2afbcd76654715b6fb9d63a6c82fddef7a5
debas-31 <debas31@gmail.com>
2022-07-26 18:23:47 +0300

    Add create method to add a recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,19 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+
+  def create
+    @new_recipe = current_user.recipes.new(recipe_params)
+    if @new_recipe.save!
+      redirect_to recipes_path, flash: { alert: 'Your recipe is saved' }
+    else 
+      redirect_to new_recipe_path, flash: { alert: 'Could not save your recipe' }
+    end
+  end
+  
+  private
+  
+  def recipe_params
+    params.require(:recipe).permit(:name, :prepration_time, :cooking_time, :description)
+  end
 end
