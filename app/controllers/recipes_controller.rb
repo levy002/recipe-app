7053faf24c532ce203fd7071993497c035c0950c
debas-31 <debas31@gmail.com>
2022-07-27 13:43:55 +0300

    Add method and routes on recipe for show

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,9 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
-  def show; end
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
 
   def create
     @new_recipe = current_user.recipes.new(recipe_params)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'recipes/index'
   devise_for :users
-  resources :recipes, only: [:index, :new, :create, :destroy]
+  resources :recipes, only: [:index, :show, :new, :create, :destroy]
   root 'users#index'
   resources :foods
 end
