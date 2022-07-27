e9a50d8d1af7adff37d03f64a685fe5f7f7f5027
debas-31 <debas31@gmail.com>
2022-07-27 18:28:18 +0300

    Add update method to recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -24,6 +24,19 @@ class RecipesController < ApplicationController
     redirect_to recipes_path
   end
 
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.public
+      @recipe.update(public: false)
+      flash[:notice] = 'You have update the recipe status to private'
+      redirect_to recipes_path
+    else
+      @recipe.update(public: true)
+      flash[:notice] = 'You have update the recipe status to public'
+      redirect_to recipes_path
+    end
+  end
+
   private
 
   def recipe_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'recipes/index'
   devise_for :users
-  resources :recipes, only: [:index, :show, :new, :create, :destroy]
+  resources :recipes, only: [:index, :show, :new, :create, :destroy, :update]
   root 'users#index'
   resources :foods
 end
