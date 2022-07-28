43afe0a9ada8e09924e08ab4866bd6c7c1c24675
levy002 <levy.ukwi002@gmail.com>
2022-07-28 19:58:21 +0200

    Fix rubocop errors

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -3,6 +3,5 @@ class GeneralShoppingListController < ApplicationController
     @recipes = Recipe.where(user_id: current_user.id)
     @ingredients = FoodRecipe.where(recipe_id: @recipes.map(&:id))
     @foods = Food.where(id: @ingredients.map(&:food_id))
-
   end
 end
