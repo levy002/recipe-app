1d37b730e7d664adc13d2755f50420e65882e77d
debas-31 <debas31@gmail.com>
2022-07-28 10:00:15 +0300

    Add route for edit method of the food-recipe

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'recipes/index'
   devise_for :users
   resources :recipes, only: [:index, :show, :new, :create, :destroy, :update] do
-    resources :food_recipes, only: [:new, :create, :destroy, :update]
+    resources :food_recipes, only: [:new, :create, :destroy, :edit, :update]
   end
   root 'users#index'
   resources :foods
