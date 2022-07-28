388ac4f0045751727cab8f4eb1ad1c7db164d18a
debas-31 <debas31@gmail.com>
2022-07-28 17:47:40 +0300

    Add validation to user and recipe models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,9 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :food_recipes, dependent: :destroy
   has_many :foods, through: :food_recipes
+
+  validates :name, presence: true, length: { in: 1..50 }
+  validates :prepration_time, presence: true, length: { in: 1..100 }
+  validates :cooking_time, presence: true, length: { in: 1..100 }
+  validates :description, presence: true, length: { in: 1..300 }
 end
