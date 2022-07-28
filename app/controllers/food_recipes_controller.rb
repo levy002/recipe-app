c1403f22f178c87a0804e47da65850dedbb9e806
levy002 <levy.ukwi002@gmail.com>
2022-07-28 15:06:00 +0200

    Add method to render only Public recipes

--- app/controllers/food_recipes_controller.rb
+++ app/controllers/food_recipes_controller.rb
@@ -15,13 +15,11 @@ class FoodRecipesController < ApplicationController
   end
 
   def create
-    # @recipe = Recipe.find(params[:recipe_id])
-    # @food_recipe = @recipe.food_recipes.create(food_recipe_params)
     @recipe = Recipe.find(params[:recipe_id])
     food_recipe = FoodRecipe.new(params.require(:recipe_food).permit(:food_id, :quantity))
     food_recipe.recipe = @recipe
     if food_recipe.save
-      redirect_to recipe_path(params[:recipe_id]), flash: { alert: 'Your food is saved' }
+      redirect_to recipe_path(params[:recipe_id]), flash: { notice: 'Your food is saved' }
     else
       redirect_to new_recipe_food_recipe_path, flash: { alert: 'Could not save your food' }
     end
