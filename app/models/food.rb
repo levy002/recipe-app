a8298633942cf6af24d9cb42d60c5fc4e2d19105
debas-31 <debas31@gmail.com>
2022-07-28 18:25:57 +0300

    Add test for food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,7 @@
 class Food < ApplicationRecord
   belongs_to :user
 
-  validates :measurement_unit, :unit_price, presence: true
-  validates :unit_price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :name, presence: true, length: { in: 1..20 }
+  validates :measurement_unit, presence: true, length: { in: 1..10 }
+  validates :unit_price, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 end
